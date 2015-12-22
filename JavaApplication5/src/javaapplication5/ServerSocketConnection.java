db967cdc7fbbb8e2cbe547d0e29adcc9bc4d8c7c
misimaLab <ai.mychronicle07@gmail.com>
2015-12-22 18:33:20 +0900

    いろいろ変更
    サーバー：クライアントIPの表示と変数の用意とメソッド

--- JavaApplication5/src/javaapplication5/ServerSocketConnection.java
+++ JavaApplication5/src/javaapplication5/ServerSocketConnection.java
@@ -5,9 +5,8 @@
  */
 package javaapplication5;
 
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.io.*;
+import java.net.*;
 
 /**
  *
@@ -16,13 +15,15 @@ import java.io.*;
 public class ServerSocketConnection {
     
     private int port_num;
+    private String client_ip;
     private String inform_sentence;
     
     
-    public ServerSocketConnection(int port_Num){
+    public ServerSocketConnection(int port_Num,String client_Ip){
         try{
             this.port_num=port_Num;
-            System.out.println("接続クライアント："+this.port_num+"/使用するポート："+this.port_num);
+            this.client_ip=client_Ip;
+            System.out.println("接続するクライアントIP："+this.client_ip+"/使用ポート："+this.port_num);
         }catch(NullPointerException e){
             System.out.println("接続する相手のアドレスまたはポート番号がうけとれませんでした");
         }
@@ -30,27 +31,42 @@ public class ServerSocketConnection {
 
     
     public void connection_start(){
+        System.out.println("▼▼▼▼▼ソケット通信開始▼▼▼▼▼");
         try{
             //サーバーソケットの生成
             ServerSocket serverSocket=new ServerSocket();
+            System.out.println("ServerSocket生成完了/ポートを監視中");
             
             //クライアントからの接続を待つ
-            Socket socket=serverSocket.accept();      
+            Socket socket=serverSocket.accept();  
             System.out.println(socket.getInetAddress()+"接続完了");
             
-            //出力ストリームを取得
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            // 入力ストリームを取得
-            InputStream	is = socket.getInputStream();
-            BufferedReader in = new BufferedReader(new InputStreamReader(is));
+            /* 年齢を受信する */
+            ObjectInputStream ois =new ObjectInputStream(socket.getInputStream());
+            Integer data = (Integer)ois.readObject();
 
+
+            /* 飲酒の可否を判定する */
+           //受け取ったデータのなんかしらの処理
+            
+            /* 判定結果をクライアントに送信する */
+            ObjectOutputStream oos =new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(data);
+            oos.flush();
         }catch(Exception e){
             e.printStackTrace();
             System.out.println("ソケットの接続が確立されていません");
         }
         
     }
-    
-    
-    
 }
+//データの送信
+//        ObjectOutputStream oos =new ObjectOutputStream(socket.getOutputStream());
+//        oos.writeObject(send_data);
+//        oos.flush();
+//
+//データの受信
+//        ObjectInputStream ois =new ObjectInputStream(socket.getInputStream());
+//        (型宣言) receive_data = (キャスト)ois.readObject();
+//        (型宣言) get_data = receive_data.intValue();
+//      
