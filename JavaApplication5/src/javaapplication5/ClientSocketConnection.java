eb92e841fc198ef3381fb4101bb53448fdb05fa5
misimaLab <ai.mychronicle07@gmail.com>
2015-12-22 18:33:31 +0900

    クライアント：サーバーIPの表示と変数の用意とメソッド
    クライアント：サーバーIPの表示と変数の用意とメソッド

--- JavaApplication5/src/javaapplication5/ClientSocketConnection.java
+++ JavaApplication5/src/javaapplication5/ClientSocketConnection.java
@@ -5,14 +5,8 @@
  */
 package javaapplication5;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
+import java.io.*;
+import java.net.*;
 /**
  *
  * @author tatoo
@@ -21,32 +15,46 @@ public class ClientSocketConnection {
     private int port_num;
     private String server_ip;
     private String inform_sentence;
+    private String send_data1;
+    private String send_data2;
+    
 
     
-    public ClientSocketConnection(int port_Num){
+    public ClientSocketConnection(int port_Num,String server_Ip){
         try{
             this.port_num=port_Num;
-            System.out.println("接続クライアント："+this.port_num+"/使用するポート："+this.port_num);
+            this.server_ip=server_Ip;
+            System.out.println("接続するサーバIP："+this.server_ip+"/使用するポート："+this.port_num);
         }catch(NullPointerException e){
             System.out.println("接続する相手のアドレスまたはポート番号がうけとれませんでした");
         }
     }
+    public String insert_data(String send_1){
+            this.send_data1=send_1;
+        return null;
+    }
+    public String insert_data(String send_Data1,String send_Data2){
+            this.send_data1=send_Data1;
+            this.send_data2=send_Data2;
+        return null;
+    }
 
     
     public void connection_start(){
+        System.out.println("▼▼▼▼▼ソケット通信開始▼▼▼▼▼");
         try{
             //サーバーソケットの生成
-            Socket Socket=new Socket();
+            Socket socket=new Socket(server_ip,port_num);
+            System.out.println("Socket生成完了/サーバーに接続要求を送信");
             
-            //クライアントからの接続を待つ
-//            Socket socket=Socket.accept();      
-//            System.out.println(socket.getInetAddress()+"接続完了");
-//            
-//            //出力ストリームを取得
-//            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-//            // 入力ストリームを取得
-//            InputStream	is = socket.getInputStream();
-//            BufferedReader in = new BufferedReader(new InputStreamReader(is));
+            /* サーバに年齢を送信する */
+            ObjectOutputStream oos =new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(send_data1);
+            oos.flush();
+
+            /* サーバから判定結果を受信する */
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            String result = (String)ois.readObject();
 
         }catch(Exception e){
             e.printStackTrace();
@@ -56,3 +64,13 @@ public class ClientSocketConnection {
 
     
 }
+//データの送信
+//        ObjectOutputStream oos =new ObjectOutputStream(socket.getOutputStream());
+//        oos.writeObject(send_data);
+//        oos.flush();
+//
+//データの受信
+//        ObjectInputStream ois =new ObjectInputStream(socket.getInputStream());
+//        (型宣言) receive_data = (キャスト)ois.readObject();
+//        (型宣言) get_data = receive_data.intValue();
+//        
