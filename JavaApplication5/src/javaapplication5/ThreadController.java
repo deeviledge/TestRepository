e5709c9849109ad63363387ace24a6b853ba0d5d
misimaLab <ai.mychronicle07@gmail.com>
2015-12-28 17:50:07 +0900

    サーバースレッドに実装したい部分

---
+++ JavaApplication5/src/javaapplication5/ThreadController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+//import sglserver.groupadmin.GroupInformationXml;
+/**
+ *
+ * @author tatoo
+ */
+/**
+ * クライアントに対応したスレッドを作成してクライアントとスレッドを制御するクラス
+ * 
+ * @author nishimura
+ *
+ */
+public class ThreadController {
+    
+
+}
+
+
+
+
